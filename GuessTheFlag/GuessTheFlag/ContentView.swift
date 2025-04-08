ea16a2b67087afb9f5b765aa766315848e8a247a
Nikita Shyshkin <yatoenough@gmail.com>
2025-04-08 23:40:59 +0200

    Added assets and implemented UI

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let countries: [String] = ["Estonia", "Ireland", "Germany", "Italy", "France", "Spain", "Poland", "US", "Ukraine", "UK", "Nigeria"]
+    
+    private var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+                
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                    
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                }
+                
+                ForEach(0..<3) { num in
+                    Button {
+                        
+                    } label: {
+                        Image(countries[num])
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 

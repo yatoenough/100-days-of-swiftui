cada04088a6b8ad554022330a6283202492fb441
Nikita Shyshkin <yatoenough@gmail.com>
2025-04-08 08:37:34 +0200

    Inited WeSplit app

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nikita Shyshkin on 08/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

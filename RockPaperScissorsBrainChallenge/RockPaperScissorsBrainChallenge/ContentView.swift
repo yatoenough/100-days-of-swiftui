911a3a2908bb3512f4c244fd84cb6ef3fd620264
Nikita Shyshkin <yatoenough@gmail.com>
2025-04-11 15:22:43 +0200

    Styled app

--- RockPaperScissorsBrainChallenge/RockPaperScissorsBrainChallenge/ContentView.swift
+++ RockPaperScissorsBrainChallenge/RockPaperScissorsBrainChallenge/ContentView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    let moves = ["rock", "paper", "scissors"]
+    private let moves = ["rock", "paper", "scissors"]
+    private let emojis: [String: String] = [
+        "rock": "🪨",
+        "paper": "📄",
+        "scissors": "✂️"
+    ]
+    
     @State var win = Bool.random()
+    @State private var currentMove: Int = Int.random(in: 0..<3)
+    @State private var score = 0
     
     private var winningMoves: [String] {
         var result: [String] = []
@@ -45,49 +53,89 @@ struct ContentView: View {
         return result
     }
     
-    @State private var currentMove: Int = Int.random(in: 0..<3)
-    @State private var score = 0
-    
     var body: some View {
-        VStack {
-            VStack {
-                Text("Score: \(score)")
-                Text("\(moves[currentMove].capitalized)")
-                Text("\(win ? "Win" : "Lose")")
-            }
+        ZStack {
+            LinearGradient(colors: [.red, .black], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
             
-            HStack {
-                ForEach(moves, id: \.self) { move in
-                    Button(move) {
-                        turn(move: move)
-                    }
+            VStack {
+                
+                Spacer()
+                
+                VStack {
+                    Text("Score: \(score)")
+                        .font(.headline)
+                        .padding(.top)
+                        .contentTransition(.numericText())
                     
+                    VStack(spacing: 20) {
+                        Text("\(emojis[moves[currentMove]]!)")
+                            .font(.largeTitle)
+                        
+                        Text("\(moves[currentMove].capitalized)")
+                            .font(.largeTitle)
+                    }
+                    .padding()
+                    
+                    
+                    Text("\(win ? "Win" : "Lose")")
+                        .font(.title2)
+                        .padding(.bottom)
+                        .foregroundStyle(win ? .green : .red)
+                        .animation(.easeInOut, value: win)
                 }
-                .padding()
+                .frame(maxWidth: .infinity)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .padding(.horizontal, 100)
+                
+                Spacer()
+                
+                HStack {
+                    ForEach(moves, id: \.self) { move in
+                        Button(emojis[move]!.capitalized) {
+                            nextTurn(move: move)
+                        }
+                        .font(.largeTitle)
+                        .padding()
+                        .background(.ultraThinMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        
+                    }
+                    .padding()
+                }
+                
+                Spacer()
             }
+            .foregroundStyle(.white)
         }
+        .bold()
     }
     
-    func turn(move: String) {
-        if win {
-            if move == winningMoves[currentMove] {
-                score += 1
+    private func nextTurn(move: String) {
+        withAnimation {
+            if win {
+                if move == winningMoves[currentMove] {
+                    score += 1
+                } else {
+                    score -= 1
+                }
+                
             } else {
-                score -= 1
+                if move == losingMoves[currentMove] {
+                    score += 1
+                } else {
+                    score -= 1
+                }
             }
             
-        } else {
-            if move == losingMoves[currentMove] {
-                score += 1
-            } else {
-                score -= 1
-            }
+            
+            resetTurn()
         }
         
-        resetTurn()
     }
     
-    func resetTurn() {
+    private func resetTurn() {
         currentMove = Int.random(in: 0..<3)
         win = Bool.random()
     }

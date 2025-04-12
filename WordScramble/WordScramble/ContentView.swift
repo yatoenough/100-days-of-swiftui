6a27e14fd81e6fb6aeb0e8e692e9d6c2ef1151e1
Nikita Shyshkin <yatoenough@gmail.com>
2025-04-12 16:29:27 +0200

    Implemented game and validation logic

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
         NavigationStack {
             List {
@@ -31,18 +35,83 @@ struct ContentView: View {
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
+            .onAppear(perform: load)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK") { }
+            } message: {
+                Text(errorMessage)
+            }
         }
     }
     
-    func addNewWord() {
+    private func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
         guard answer.count > 0 else { return }
+        
+        guard isOriginal(answer) else {
+            wordError(title: "Word used already", message: "Try another one!")
+            return
+        }
+        
+        guard isPossible(answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
         
         withAnimation {
             usedWords.insert(answer, at: 0)
             newWord = ""
         }
+    }
+    
+    private func load() {
+        if let wordsFileURL = Bundle.main.url(forResource: "words", withExtension: "txt") {
+            if let words = try? String(contentsOf: wordsFileURL, encoding: .utf8) {
+                let allWords = words.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "bookworm"
+                return
+            }
+        }
+        
+        fatalError("Couldn't load words file from bundle.")
+    }
+    
+    private func isOriginal(_ word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    private func isPossible(_ word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    private func isReal(_ word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    private func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 

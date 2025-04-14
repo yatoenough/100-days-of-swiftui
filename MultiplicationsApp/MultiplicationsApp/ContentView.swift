11e1f669e5cb0f5b2c235e83bdde2e7f369f1b22
Nikita Shyshkin <yatoenough@gmail.com>
2025-04-14 16:01:40 +0200

    Inited app

---
+++ MultiplicationsApp/MultiplicationsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultiplicationsApp
+//
+//  Created by Nikita Shyshkin on 14/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultiplicationsApp/MultiplicationsApp/MultiplicationsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationsAppApp.swift
+//  MultiplicationsApp
+//
+//  Created by Nikita Shyshkin on 14/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

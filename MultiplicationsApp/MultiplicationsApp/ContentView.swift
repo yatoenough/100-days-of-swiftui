35a20832c391fa024a00a494be70a89bdd2babf2
Nikita Shyshkin <yatoenough@gmail.com>
2025-04-15 10:25:19 +0200

    Created Start Form with settings

--- MultiplicationsApp/MultiplicationsApp/ContentView.swift
+++ MultiplicationsApp/MultiplicationsApp/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var upTo = 2
+    @State private var questionsCount = 5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(
+                colors: [.yellow, .red, .blue],
+                startPoint: .bottomTrailing,
+                endPoint: .topLeading
+            )
+            .ignoresSafeArea()
+            
+            StartForm(upTo: $upTo, questionsCount: $questionsCount) {
+                
+            }
         }
-        .padding()
     }
 }
 
---
+++ MultiplicationsApp/MultiplicationsApp/StartForm.swift
@@ -0,0 +1,69 @@
+//
+//  StartForm.swift
+//  MultiplicationsApp
+//
+//  Created by Nikita Shyshkin on 15/04/2025.
+//
+
+import SwiftUI
+
+struct StartForm: View {
+    @Binding var upTo: Int
+    @Binding var questionsCount: Int
+    
+    let onStart: () -> Void
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 30) {
+            Text("Multiplication")
+                .font(.largeTitle)
+            
+            Stepper("Up to \(upTo)", value: $upTo, in: 2...12)
+            
+            HStack {
+                Text("How many questions?")
+                
+                Spacer()
+                
+                Picker("How many questions?", selection: $questionsCount) {
+                    Text("5")
+                        .tag(5)
+                    
+                    Text("10")
+                        .tag(10)
+                    
+                    Text("20")
+                        .tag(20)
+                }
+                .tint(.white)
+                .pickerStyle(.segmented)
+            }
+            
+            
+            Button("Tap to start", action: onStart)
+                .buttonStyle(.borderedProminent)
+                .tint(.white.opacity(0.3))
+                .scaleEffect(1.5)
+                .padding()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .shadow(radius: 5, x: 0, y: 10)
+        .foregroundColor(.white)
+        .bold()
+        .padding()
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    @Previewable @State var upTo = 2
+    @Previewable @State var questionsCount = 5
+    
+    StartForm(upTo: $upTo, questionsCount: $questionsCount) {
+        
+    }
+    .background(.black)
+}
+
+

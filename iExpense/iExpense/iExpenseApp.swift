255a4a666e3bc0180a28566623dcd97533c1dcc5
Nikita Shyshkin <yatoenough@gmail.com>
2025-04-17 11:41:50 +0200

    Inited app

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Nikita Shyshkin on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

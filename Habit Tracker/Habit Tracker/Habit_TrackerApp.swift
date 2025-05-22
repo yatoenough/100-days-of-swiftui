66d99e887e57213c4b0817294d92b8bbb2fbe381
Nikita Shyshkin <yatoenough@gmail.com>
2025-05-22 14:32:12 +0200

    Inited new app for challenge

---
+++ Habit Tracker/Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Nikita Shyshkin on 22/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

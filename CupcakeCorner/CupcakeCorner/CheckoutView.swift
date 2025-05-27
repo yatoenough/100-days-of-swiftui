f3ea1f3cc4d84dcfab5e1ee29670f847e38ec36d
Nikita Shyshkin <yatoenough@gmail.com>
2025-05-27 10:16:03 +0200

    Requesting dummy API

--- CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct CheckoutView: View {
 	var order: Order
 	
+	@State private var confirmationMessage = ""
+	@State private var showingConfirmation = false
+	
     var body: some View {
 		ScrollView {
 			VStack {
@@ -25,14 +28,44 @@ struct CheckoutView: View {
 				Text("Your total is \(order.cost, format: .currency(code: "USD"))")
 					.font(.title)
 
-				Button("Place Order", action: { })
+				Button("Place Order") {
+					Task {
+						await placeOrder()
+					}
+				}
 					.padding()
 			}
 		}
 		.navigationTitle("Check out")
 		.navigationBarTitleDisplayMode(.inline)
 		.scrollBounceBehavior(.basedOnSize)
+		.alert("Thank you!", isPresented: $showingConfirmation) { } message: {
+			Text(confirmationMessage)
+		}
     }
+	
+	func placeOrder() async {
+		guard let encoded = try? JSONEncoder().encode(order) else {
+			print("Failed to encode order")
+			return
+		}
+		
+		let url = URL(string: "https://reqres.in/api/cupcakes")!
+		var request = URLRequest(url: url)
+		
+		request.httpMethod = "POST"
+		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+		request.setValue("reqres-free-v1", forHTTPHeaderField: "x-api-key")
+		
+		do {
+			let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+			
+			let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+			confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way"
+		} catch {
+			print("Checkout failed: \(error.localizedDescription)")
+		}
+	}
 }
 
 #Preview {
--- CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -8,7 +8,19 @@
 import Foundation
 
 @Observable
-class Order {
+class Order: Codable {
+	enum CodingKeys: String, CodingKey {
+		case _type = "type"
+		case _quantity = "quantity"
+		case _specialRequestEnabled = "specialRequestEnabled"
+		case _extraFrosting = "extraFrosting"
+		case _addSprinkles = "addSprinkles"
+		case _name = "name"
+		case _city = "city"
+		case _streetAddress = "streetAddress"
+		case _zip = "zip"
+	}
+	
 	static let types: [String] = ["Vanilla", "Chocolate", "Strawberry", "Rainbow"]
 	
 	var name = ""
@@ -58,4 +70,6 @@ class Order {
 
 		return cost
 	}
+	
+	
 }

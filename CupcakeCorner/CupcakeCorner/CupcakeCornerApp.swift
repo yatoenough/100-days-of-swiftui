5e37e230d29eb64ca4b2881a709a936792adaf2b
Nikita Shyshkin <yatoenough@gmail.com>
2025-05-27 09:12:19 +0200

    Created new app and basic screens

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Nikita Shyshkin on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,61 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Nikita Shyshkin on 27/05/2025.
+//
+
+import Foundation
+
+@Observable
+class Order {
+	static let types: [String] = ["Vanilla", "Chocolate", "Strawberry", "Rainbow"]
+	
+	var name = ""
+	var streetAddress = ""
+	var city = ""
+	var zip = ""
+	
+	var type = 0
+	var quantity = 3
+	
+	var specialRequestEnabled = false {
+		didSet {
+			if !specialRequestEnabled {
+				extraFrosting = false
+				addSprinkles = false
+			}
+		}
+	}
+	
+	var extraFrosting = false
+	var addSprinkles = false
+	
+	var hasValidAddress: Bool {
+		if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+			return false
+		}
+
+		return true
+	}
+	
+	var cost: Decimal {
+		// $2 per cake
+		var cost = Decimal(quantity) * 2
+
+		// complicated cakes cost more
+		cost += Decimal(type) / 2
+
+		// $1/cake for extra frosting
+		if extraFrosting {
+			cost += Decimal(quantity)
+		}
+
+		// $0.50/cake for sprinkles
+		if addSprinkles {
+			cost += Decimal(quantity) / 2
+		}
+
+		return cost
+	}
+}

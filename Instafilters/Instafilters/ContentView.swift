dab1b3a352487c2121737d1b24abd5440a8df288
Nikita Shyshkin <yatoenough@gmail.com>
2025-06-12 23:32:48 +0200

    Inited new app

---
+++ Instafilters/Instafilters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Instafilters
+//
+//  Created by Nikita Shyshkin on 12/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instafilters/Instafilters/InstafiltersApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafiltersApp.swift
+//  Instafilters
+//
+//  Created by Nikita Shyshkin on 12/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstafiltersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

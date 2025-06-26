6d0969ef102f803f0189f99c493c560909cf57a6
Nikita Shyshkin <yatoenough@gmail.com>
2025-06-26 22:52:50 +0200

    Init new app

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Nikita Shyshkin on 26/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

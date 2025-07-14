f1080b44c95e11bae027cb9a84503112a77dfb79
Nikita Shyshkin <yatoenough@gmail.com>
2025-07-14 18:32:24 +0200

    Change map style

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -10,35 +10,64 @@ import SwiftUI
 
 struct ContentView: View {
 	@State private var viewModel = ViewModel()
-	
+	@State private var mapHybridStyle = false
+
 	var body: some View {
 		if viewModel.isUnlocked {
 			MapReader { proxy in
-				Map {
-					ForEach(viewModel.locations) { location in
-						Annotation(location.name, coordinate: location.coordinate) {
-							Image(systemName: "star.circle")
-								.resizable()
-								.foregroundStyle(.red)
-								.frame(width: 44, height: 44)
-								.background(.white)
-								.clipShape(.circle)
-								.onLongPressGesture(minimumDuration: 0.2) {
-									viewModel.selectedPlace = location
-								}
+				ZStack {
+					Map {
+						ForEach(viewModel.locations) { location in
+							Annotation(
+								location.name,
+								coordinate: location.coordinate
+							) {
+								Image(systemName: "star.circle")
+									.resizable()
+									.foregroundStyle(.red)
+									.frame(width: 44, height: 44)
+									.background(.white)
+									.clipShape(.circle)
+									.onLongPressGesture(minimumDuration: 0.2) {
+										viewModel.selectedPlace = location
+									}
+							}
+
 						}
-						
+
 					}
-				}
-				.onTapGesture { position in
-					if let coordinate = proxy.convert(position, from: .local) {
-						viewModel.addLocation(at: coordinate)
+					.mapStyle(mapHybridStyle ? .hybrid : .standard)
+					.onTapGesture { position in
+						if let coordinate = proxy.convert(
+							position,
+							from: .local
+						) {
+							viewModel.addLocation(at: coordinate)
+						}
 					}
-				}
-				.sheet(item: $viewModel.selectedPlace) { place in
-					EditView(location: place) {
-						viewModel.update(location: $0)
+					.sheet(item: $viewModel.selectedPlace) { place in
+						EditView(location: place) {
+							viewModel.update(location: $0)
+						}
 					}
+					VStack(alignment: .trailing) {
+						Spacer()
+						HStack {
+							Spacer()
+							
+							Button {
+								mapHybridStyle.toggle()
+							} label: {
+								Image(systemName: "map.circle.fill")
+							}
+							.padding()
+							.background(.blue.gradient)
+							.foregroundStyle(.white)
+							.clipShape(.capsule)
+						}
+					}
+					.padding()
+
 				}
 			}
 		} else {

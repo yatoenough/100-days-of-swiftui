ebb3f306b6724f6e8b1f7419874740f1f337c9b6
Nikita Shyshkin <yatoenough@gmail.com>
2025-07-15 13:17:46 +0200

    Init new app

---
+++ AccessibilityDemo/AccessibilityDemo/AccessibilityDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityDemoApp.swift
+//  AccessibilityDemo
+//
+//  Created by Nikita Shyshkin on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

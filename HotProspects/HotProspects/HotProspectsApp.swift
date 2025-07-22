8d095b67f5e793131c75cd9cb6147a49578cef1c
Nikita Shyshkin <yatoenough@gmail.com>
2025-07-23 00:44:36 +0200

    Init new app

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Nikita Shyshkin on 23/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

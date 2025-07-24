3d95b970140fdc95b918d82e613a85ae59165907
Nikita Shyshkin <yatoenough@gmail.com>
2025-07-24 21:27:39 +0200

    Add swipe actions and delete feature

--- HotProspects/HotProspects/ProspectsView.swift
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -19,6 +19,7 @@ struct ProspectsView: View {
 	@Query(sort: \Prospect.name) var prospects: [Prospect]
 
 	@State private var isShowingScanner = false
+	@State private var selectedProspects = Set<Prospect>()
 
 	let filter: FilterType
 
@@ -50,7 +51,7 @@ struct ProspectsView: View {
 
 	var body: some View {
 		NavigationStack {
-			List(prospects) { prospect in
+			List(prospects, selection: $selectedProspects) { prospect in
 				VStack(alignment: .leading) {
 					Text(prospect.name)
 						.font(.headline)
@@ -58,11 +59,39 @@ struct ProspectsView: View {
 					Text(prospect.email)
 						.foregroundStyle(.secondary)
 				}
+				.swipeActions {
+					Button("Delete", systemImage: "trash", role: .destructive, action: delete)
+					
+					if prospect.isContacted {
+						Button("Uncontact", systemImage: "person.crop.circle.badge.xmark") {
+							prospect.isContacted.toggle()
+						}
+						.tint(.blue)
+					} else {
+						Button("Contact", systemImage: "person.crop.circle.fill.badge.checkmark") {
+							prospect.isContacted.toggle()
+						}
+						.tint(.green)
+					}
+				}
+				.tag(prospect)
 			}
 			.navigationTitle(title)
 			.toolbar {
-				Button("Scan", systemImage: "qrcode.viewfinder") {
-					isShowingScanner = true
+				ToolbarItem(placement: .topBarTrailing) {
+					Button("Scan", systemImage: "qrcode.viewfinder") {
+						isShowingScanner = true
+					}
+				}
+				
+				ToolbarItem(placement: .topBarLeading) {
+					EditButton()
+				}
+				
+				if selectedProspects.isEmpty == false {
+					ToolbarItem(placement: .bottomBar) {
+						Button("Delete Selected", action: delete)
+					}
 				}
 			}
 			.sheet(isPresented: $isShowingScanner) {
@@ -88,6 +117,12 @@ struct ProspectsView: View {
 			
 		case .failure(let error):
 			print("Scanning failed: \(error.localizedDescription)")
+		}
+	}
+	
+	func delete() {
+		for prospect in selectedProspects {
+			modelContext.delete(prospect)
 		}
 	}
 }

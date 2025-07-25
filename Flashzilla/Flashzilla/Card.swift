677f47f5258667284c51fbded48263ac79b5425f
Nikita Shyshkin <yatoenough@gmail.com>
2025-07-25 17:06:02 +0200

    Created card view and implemented card swiping

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,17 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Nikita Shyshkin on 25/07/2025.
+//
+
+import Foundation
+
+struct Card {
+	var prompt: String
+	var answer: String
+	
+	#if DEBUG
+	static let example = Card(prompt: "What is Swift?", answer: "A high-level programming language.")
+	#endif
+}
--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -7,18 +7,42 @@
 
 import SwiftUI
 
+extension View {
+	func stacked(at position: Int, in total: Int) -> some View {
+		let offset = Double(total - position)
+		return self.offset(y: offset * 10)
+	}
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	@State private var cards = Array(repeating: Card.example, count: 10)
+
+	var body: some View {
+		ZStack {
+			Image(.background)
+				.resizable()
+				.ignoresSafeArea()
+
+			VStack {
+				ZStack {
+					ForEach(0..<cards.count, id: \.self) { index in
+						CardView(card: cards[index]) {
+							withAnimation {
+								removeCard(at: index)
+							}
+						}
+						.stacked(at: index, in: cards.count)
+					}
+				}
+			}
+		}
+	}
+
+	func removeCard(at index: Int) {
+		cards.remove(at: index)
+	}
 }
 
 #Preview {
-    ContentView()
+	ContentView()
 }

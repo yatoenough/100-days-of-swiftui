a8db480eb8e4f5e72a589bee5a01349c3f740122
Nikita Shyshkin <yatoenough@gmail.com>
2025-07-25 20:24:47 +0200

    Init new app

---
+++ Layouts/Layouts/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Layouts
+//
+//  Created by Nikita Shyshkin on 25/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+		Text("Hello, world!")
+			.background(.red)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Layouts/Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Nikita Shyshkin on 25/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

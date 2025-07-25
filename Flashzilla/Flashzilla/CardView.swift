37c7a6391350fc0bed38f8030d28e2d9e525b58b
Nikita Shyshkin <yatoenough@gmail.com>
2025-07-25 18:08:39 +0200

    Bugfix and accessibility adaptation

--- Flashzilla/Flashzilla/CardView.swift
+++ Flashzilla/Flashzilla/CardView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct CardView: View {
 	let card: Card
-	
+
 	var removal: (() -> Void)? = nil
 
 	@Environment(\.accessibilityDifferentiateWithoutColor)
 	var accessibilityDifferentiateWithoutColor
+	
+	@Environment(\.accessibilityVoiceOverEnabled)
+	var accessibilityVoiceOverEnabled
 
 	@State private var isShowingAnswer = false
 	@State private var offset = CGSize.zero
@@ -35,14 +38,20 @@ struct CardView: View {
 				.shadow(radius: 10)
 
 			VStack {
-				Text(card.prompt)
-					.font(.largeTitle)
-					.foregroundStyle(.black)
+				if accessibilityVoiceOverEnabled {
+					Text(isShowingAnswer ? card.answer : card.prompt)
+						.font(.largeTitle)
+						.foregroundStyle(.black)
+				} else {
+					Text(card.prompt)
+						.font(.largeTitle)
+						.foregroundStyle(.black)
 
-				if isShowingAnswer {
-					Text(card.answer)
-						.font(.title)
-						.foregroundStyle(.secondary)
+					if isShowingAnswer {
+						Text(card.answer)
+							.font(.title)
+							.foregroundStyle(.secondary)
+					}
 				}
 			}
 			.padding(20)
@@ -52,6 +61,7 @@ struct CardView: View {
 		.rotationEffect(.degrees(offset.width / 5.0))
 		.offset(x: offset.width * 5)
 		.opacity(2 - Double(abs(offset.width / 50)))
+		.accessibilityAddTraits(.isButton)
 		.gesture(
 			DragGesture()
 				.onChanged { gesture in
--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -17,6 +17,9 @@ extension View {
 struct ContentView: View {
 	@Environment(\.accessibilityDifferentiateWithoutColor)
 	var accessibilityDifferentiateWithoutColor
+	
+	@Environment(\.accessibilityVoiceOverEnabled)
+	var accessibilityVoiceOverEnabled
 
 	@State private var cards = Array(repeating: Card.example, count: 10)
 	@State private var timeRemaining = 100
@@ -28,7 +31,7 @@ struct ContentView: View {
 
 	var body: some View {
 		ZStack {
-			Image(.background)
+			Image(decorative: "background")
 				.resizable()
 				.ignoresSafeArea()
 
@@ -49,6 +52,8 @@ struct ContentView: View {
 							}
 						}
 						.stacked(at: index, in: cards.count)
+						.allowsHitTesting(index == cards.count - 1)
+						.accessibilityHidden(index < cards.count - 1)
 					}
 				}
 				.allowsHitTesting(timeRemaining > 0)
@@ -62,22 +67,38 @@ struct ContentView: View {
 				}
 			}
 
-			if accessibilityDifferentiateWithoutColor {
+			if accessibilityDifferentiateWithoutColor || accessibilityVoiceOverEnabled {
 				VStack {
 					Spacer()
 
 					HStack {
-						Image(systemName: "xmark.circle")
-							.padding()
-							.background(.black.opacity(0.7))
-							.clipShape(.circle)
-
+						Button {
+							withAnimation {
+								removeCard(at: cards.count - 1)
+							}
+						} label: {
+							Image(systemName: "xmark.circle")
+								.padding()
+								.background(.black.opacity(0.7))
+								.clipShape(.circle)
+						}
+						.accessibilityLabel("Wrong")
+						.accessibilityHint("Mark your answer as being incorrect")
+						
 						Spacer()
-
-						Image(systemName: "checkmark.circle")
-							.padding()
-							.background(.black.opacity(0.7))
-							.clipShape(.circle)
+						
+						Button {
+							withAnimation {
+								removeCard(at: cards.count - 1)
+							}
+						} label: {
+							Image(systemName: "checkmark.circle")
+								.padding()
+								.background(.black.opacity(0.7))
+								.clipShape(.circle)
+						}
+						.accessibilityLabel("Correct")
+						.accessibilityHint("Mark your answer as being correct")
 					}
 					.foregroundStyle(.white)
 					.font(.largeTitle)
@@ -105,6 +126,8 @@ struct ContentView: View {
 	}
 
 	func removeCard(at index: Int) {
+		guard index >= 0 else { return }
+		
 		cards.remove(at: index)
 		
 		if cards.isEmpty {
